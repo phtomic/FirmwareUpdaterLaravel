d0b9dd38affdfc0fccdba2693c843bbb5a1f4e6b
Sebastião Jonas <37277259+phtomic@users.noreply.github.com>
2021-09-23 07:58:58 -0300

    Update and rename routes/web.php to rotas/web.php

--- rotas/web.php
+++ rotas/web.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+  //  return view('welcome');
+//});
